a11f36eeebd33a70beeca6e950e2d20801f47069
ElMonoSka <elmonoska@outlook.com>
2024-03-09 20:01:16 -0600

    add first cumpliment

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const imgMagicCat = document.querySelector("#magicCat");
+const btnLove = document.querySelector("#love");
+btnLove.addEventListener("click", () => {
+  console.log("Hola")
+  imgMagicCat.style.display = "block";
+})
