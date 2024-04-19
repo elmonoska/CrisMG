b173aa5545f8c8a90b5c62021f7edd258f0d64f1
Raul CF <elmonoska@outlook.com>
2024-04-19 10:34:51 -0600

    starting project with astro and tailwind

--- assets/js/dont-be-sad.js
+++
@@ -1,6 +0,0 @@
-const imgMagicCat = document.querySelector("#magicCat");
-const btnLove = document.querySelector("#love");
-btnLove.addEventListener("click", () => {
-  btnLove.textContent = "Magia ✨"
-  imgMagicCat.style.display = "block";
-})
